0ff98721918891333870b72436cd0cc3f0900f8a
smithros <kovalr2000@gmail.com>
2019-05-26 18:10:56 +0300

    added new 10 lab

---
+++ Lab10_new/longop.h
@@ -0,0 +1,6 @@
+extern "C"
+{
+	void Add_LONGOP(long bits, long *dest, long *pB, long *pA);
+	void Sub_LONGOP(long bits, long *dest, long *pB, long *pA);
+	void Mul_LONGOP(long bits, long *dest, long *pB, long *pA);
+}
